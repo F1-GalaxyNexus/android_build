dbca978957adfef1c7d106ae578d34844fd262c2
Faryaab Sheikh <faryaabsheikh@gmail.com>
2012-04-03 03:04:39 +0500

    Removed apns-conf.xml

--- target/board/generic/device.mk
+++ target/board/generic/device.mk
@@ -23,7 +23,6 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.adb.qemud=1
 
 PRODUCT_COPY_FILES := \
-    development/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
     development/data/etc/vold.conf:system/etc/vold.conf \
     development/tools/emulator/system/camera/media_profiles.xml:system/etc/media_profiles.xml \
 
