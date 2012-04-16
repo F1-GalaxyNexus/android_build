60e53fa79693e7270a1611ba1e11c01c90ba9743
Faryaab Sheikh <faryaabsheikh@gmail.com>
2012-04-16 13:41:48 +0100

    Introduce F1 GalaxyNexus Branding and set en_GB as default language

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -34,7 +34,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.notification_sound=pixiedust.ogg
 
 # Put en_US first in the list, so make it default.
-PRODUCT_LOCALES := en_US
+PRODUCT_LOCALES := en_GB
 
 # Get some sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
